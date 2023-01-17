b5af3d03fbba608271ab15ec95ec299d9148c1a1
abeaupre <beaupre-laperriere.alexis@ouranos.ca>
2023-01-17 15:03:12 -0500

    ajout fonctionnalite basic Dataset

--- spirograph/matplotlib/timeseries.py
+++ spirograph/matplotlib/timeseries.py
@@ -8,46 +8,55 @@ import matplotlib.pyplot as plt
 # To add
 #  language
 #  smoothing
-# m??anual mode: asks for input for title,
+# manual mode?
+# logo
+# default value for attributes to use?
 
 
 ## idees pour labels: arg replace_label
 
 
-def line_ts(da, ax=None, use_attrs=None, sub_kw=None, line_kw=None):
+def line_ts(data, ax=None, use_attrs=None, sub_kw=None, line_kw=None):
     """
     Plots unique time series from dataframe
-
     Args:
         ax: user-specified matplotlib axis
-        data: Xarray DataArray containing the data to plot
-        dict_metadata: dict linking a plot element (key, e.g. 'title')
+        da: Xarray DataArray containing the data to plot
+        use_attrs: dict linking a plot element (key, e.g. 'title')
             to a DataArray attribute (value, e.g. 'Description')
         sub_kw: matplotlib subplot kwargs
         line_kw : maplotlib or xarray line kwargs
-
     Returns:
         matplotlib axis
     """
-    #return empty dicts if no kwargs
     kwargs = empty_dict({'sub_kw': sub_kw, 'line_kw': line_kw})
 
-    #initialize fig, ax if ax not provided
     if not ax:
         fig, ax = plt.subplots(**kwargs['sub_kw'])
 
+    #arrange data
+    plot_dict = {}
+    if str(type(data)) == "<class 'xarray.core.dataset.Dataset'>":
+        print('DATASET DETECTED')
+        for k,v in data.data_vars.items():
+            plot_dict[k] = v
+    else:
+        plot_dict[data.name] = data
+
+    print(plot_dict)
+
     #plot
-    line_1 = da.plot.line(ax=ax, **kwargs['line_kw'])
-    #line_1.set_label()
+    for name, xr in plot_dict.items():
+        #da.plot.line(ax=ax, **kwargs['line_kw']) # using xarray plotting
+        ax.plot(xr[xr.dims[0]], xr.values, label = name) #assumes the only dim is time
+
 
     #add/modify plot elements
     if use_attrs:
-        ax_dict_metadata(ax, use_attrs, da)
+        set_plot_attrs(use_attrs, data, ax)
 
     return ax
 
-
-
-#out of function
+#test
 
 line_ts(da_pct, use_attrs= {'title': 'ccdp_name'})
--- spirograph/matplotlib/util_fcts.py
+++ spirograph/matplotlib/util_fcts.py
@@ -7,97 +7,40 @@ def empty_dict(kwargs):
             kwargs[k] = {}
     return kwargs
 
-def get_metadata(xr_obj, str):
+def get_attributes(xr_obj, str):
     """
-    Fetches attributes corresponding to their key from Xarray objects
-
+    Fetches attributes corresponding to keys from Xarray objects
     Args:
-        xr: Xarray DataArray or Dataset
+        xr_obj: Xarray DataArray or Dataset
         str: string corresponding to an attribute key
-
     Returns:
          Xarray attribute value as string
     """
-    if str in xr_obj.attrs:
+if str in xr_obj.attrs:
         return xr_obj.attrs[str]
     else:
-        raise Exception('Metadata "{0}" not found in "{1}"'.format(str, xr_obj.name))
-    #if str in xr.coords: pas sur si peut vraiment faire de quoi si dans coords .... plutôt loop a l'extérieur
+        raise Exception('Attribute "{0}" not found in "{1}"'.format(str, xr_obj.name))
 
-def ax_dict_metadata(ax, use_attrs, xr_obj):
-    if 'title' in use_attrs:
-        ax.set_title(get_metadata(xr_obj, use_attrs['title']), wrap=True)
-    if 'xlabel' in use_attrs:
-        ax.set_xlabel(get_metadata(xr_obj, use_attrs['xlabel'])) #rotation?
-    if 'ylabel' in use_attrs:
-        ax.set_ylabel(get_metadata(xr_obj, use_attrs['ylabel']))
-    return ax
-
-#transform ds into df
-def xr_pd(xr):
-    if "Dataset" in str(type(xr)):
-        return xr.to_dataframe().reset_index()
-    else:
-        return xr.to_dataframe(name='values').reset_index()
-
-
-def da_time_serie_line(da, ax=None, dict_metadata=None, sub_kw=None, line_kw=None, logo=False):
+def set_plot_attrs(attr_dict, xr_obj, ax):
     """
-    plot unique time serie from dataset
-    da: dataset xarray
-    ax: matplotlib axis
-    dict_metadata: join figure element to xarray dataset element
-    sub_kw: matplotlib subplot kwargs
-    line_kw : maplotlib line kwargs
+    Sets plot elements according to DataArray attributes. Uses get_attributes()
+    Args:
+        use_attrs (dict): dict containing specified attribute keys
+        xr_obj: Xarray DataArray
+        ax: matplotlib axis
+    Returns:
+        matplotlib axis
     """
-    kwargs = empty_dic({'sub_kw': sub_kw, 'line_kw': line_kw})
-    if not ax:
-        fig, ax = plt.subplots(**kwargs['sub_kw'])
-    da.plot.line(ax=ax, **kwargs['line_kw'])
-    if dict_metadata:
-        ax_dict_metadata(ax, dict_metadata, da, 'lines')
-    if 'label' in dict_metadata:
-        ax.legend()
+    if 'title' in attr_dict:
+        ax.set_title(get_attributes(xr_obj, attr_dict['title']), wrap=True)
+    if 'xlabel' in attr_dict:
+        ax.set_xlabel(get_attributes(xr_obj, attr_dict['xlabel'])) #rotation?
+    if 'ylabel' in attr_dict:
+        ax.set_ylabel(get_attributes(xr_obj, attr_dict['ylabel']))
     return ax
 
 
 
-def ens_time_serie(dict_xr, ax=None, dict_metadata=None, sub_kw=None, line_kwargs=None):
-    """
-    dict of xr object: only one no legend, more than one legend
-    if dictionnary, the keys will be used to name the ensembles
 
-    if to be created over coord (ex: horizon.... fait quoi?)
 
-    """
-    kwargs = empty_dic({'sub_kw': sub_kw, 'line_kwargs': line_kwargs})
-    if not ax:
-        fig, ax = plt.subplots(**kwargs['sub_kw'])
 
-    if type(dict_xr) != dict:
-        dict_xr = {'one': dict_xr}
-
-    if len(dict_xr) == 1:
-        df = xr_pd(list(dict_xr.values())[0]).drop(columns=['lat', 'lon'])
-        if "Dataset" in str(type(list(dict_xr.values())[0])):
-            df = pd.melt(df, ['time'])
-        sns.lineplot(data=df, x='time', y='value', ax=ax,  **kwargs['line_kwargs'])
-    else:
-        n = 0
-        for k, v in dict_xr.items():
-            df = xr_pd(v).drop(columns=['lat', 'lon'])
-            if "Dataset" in str(type(v)):
-                df = pd.melt(df, ['time'], value_name=k)
-            else:
-                df = df.rename(columns={'values': k})
-            if n == 0:
-                dfa = df
-            else:
-                dfa[k] = df[k]
-            n = n+1
-        dfa = pd.melt(dfa, ['time'], list(dict_xr.keys()))
-        sns.lineplot(data=dfa, x='time', y='value', hue='variable', ax=ax, **kwargs['line_kwargs'])  #ajouter palette horizon si possible - option rcp/ssp ou détecter automatique?
-    if dict_metadata:
-        ax_dict_metadata(ax, dict_metadata, list(dict_xr.values())[0], 'line')
-    return ax
-##### Fin code Sarah-Claude #####
