d97de080f376efac6854c3f25042d6bd7ab68790
sarahclaude <sarah.claude.bg@gmail.com>
2023-03-22 13:48:42 -0400

    "Added __init__.py to access functions in matplotlib"

--- spirograph/__init__.py
+++ spirograph/__init__.py
@@ -1,6 +1,6 @@
 """Top-level package for spirograph."""
 
-__author__ = """Sarah-Claude"""
+__author__ = """Sarah-Claude Bourdeau-Goulet"""
 __email__ = "bourdeau-goulet.sarah-claude@ouranos.ca"
 __version__ = "0.1.0"
 
