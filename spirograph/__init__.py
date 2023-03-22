176ad7a311ff3fb68673803302f392ddc64d83dc
Sarah-Claude Bourdeau <83776457+sarahclaude@users.noreply.github.com>
2023-03-22 13:45:07 -0400

    Update spirograph/__init__.py
    Co-authored-by: Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>

--- spirograph/__init__.py
+++ spirograph/__init__.py
@@ -4,6 +4,4 @@ __author__ = """Sarah-Claude"""
 __email__ = "bourdeau-goulet.sarah-claude@ouranos.ca"
 __version__ = "0.1.0"
 
-__all__ = (
-    # sub-packages
-    "matplotlib")
+from . import matplotlib
