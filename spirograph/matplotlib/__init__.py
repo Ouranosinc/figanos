a03d7c613ccce9f5cff172c3b2add9db1845a323
abeaupre <beaupre-laperriere.alexis@ouranos.ca>
2023-04-28 10:45:40 -0400

    init

--- spirograph/matplotlib/__init__.py
+++ spirograph/matplotlib/__init__.py
@@ -1,2 +1,2 @@
-from .plot import gdfmap, gridmap, heatmap, stripes, timeseries, violin
+from .plot import gdfmap, gridmap, heatmap, stripes, taylordiagram, timeseries, violin
 from .utils import categorical_colors, set_mpl_style
