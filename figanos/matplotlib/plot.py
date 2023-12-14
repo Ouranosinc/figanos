b968a811e0cbfe64b9b65508bbfdaa1ec1f6983d
juliettelavoie <juliette.lavoie@hotmail.ca>
2023-12-14 15:44:23 -0500

    doc

--- figanos/matplotlib/plot.py
+++ figanos/matplotlib/plot.py
@@ -2143,7 +2143,7 @@ def partition(
     for u in data.uncertainty.values:
         if u not in ["total", "variability"]:
             present_y = past_y + data.sel(uncertainty=u)
-            num = len(data.sel(uncertainty=u).elements.values.tolist())
+            num = len(data.attrs[u])
             label = f"{u} ({num})" if show_num else u
             ax.fill_between(
                 time, past_y, present_y, label=label, **fill_kw.get(u, fk_direct)
